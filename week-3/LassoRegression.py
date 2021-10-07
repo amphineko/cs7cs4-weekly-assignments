741892082c943e35f50e918a5da422ad2eaa5dee
amphineko <sukareki@gmail.com>
2021-10-07 21:13:11 +0000

    feat(lasso): fit train dataset and score with test dataset
    Signed-off-by: amphineko <sukareki@gmail.com>

--- week-3/LassoRegression.py
+++ week-3/LassoRegression.py
@@ -1,14 +1,28 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+
 from load import load_dataset
+
+polyFx = PolynomialFeatures(degree=5)
 
 if __name__ == '__main__':
     data = load_dataset()
-    x, y = data[:, [0, 1]], data[:, 2]
-    print(x.shape)
+    x, y = data[:, :-1], data[:, -1]
 
-    ax = plt.axes(projection='3d')
-    ax.scatter(x[:, 0], x[:, 1], y)
+    # create additional polynomial features
+    x = polyFx.fit_transform(x, y)
 
-    plt.show()
+    # split train and test dataset
+    x, x_test, y, y_test = train_test_split(x, y, test_size=0.1, shuffle=False)
+
+    # create and fit the Lasso model
+    lasso = Lasso(alpha=0.025)
+    lasso.fit(x, y)
+
+    # score the model
+    score = lasso.score(x_test, y_test)
+    print('Score: ', score)
